119553660ff95ee21a80990916df38ab02f0931c
Emma Konrad <emma.konrad92@gmail.com>
2017-09-19 23:45:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget(target) {
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list')
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = ((parseInt(lis[i].innerHTML)) + n)
+  }
+
+  return lis
+}
+
+function deepestChild() {
+  const deep = document.getElementById('grand-node').querySelectorAll('div')
+
+  return deep[deep.length - 1];
+}

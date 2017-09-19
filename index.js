bb902854938096bb56419cecbe758d8f479b0080
Emma Konrad <emma.konrad92@gmail.com>
2017-09-19 23:16:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget(target) {
+  return document.getElementById('nested').querySelector('div.target');
+}
